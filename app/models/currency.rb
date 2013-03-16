235c7a056df5a639bdf7945dd5178c556318b168
Adam Konner <adam.konner@gmail.com>
2013-03-16 18:19:12 -0400

    explicit table names
    (required on RHEL server)

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,4 @@
 class Currency < ActiveRecord::Base
+  self.table_name = 'Currencies'
   attr_accessible :Country, :Description, :Name, :Symbol
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,4 +1,5 @@
 class Translation < ActiveRecord::Base
+  self.table_name = 'Translations'
   belongs_to :Language, :foreign_key => 'Language'
   attr_accessible :RemoteColumnName, :RemoteID, :RemoteTable, :TranslatedContent
 end
