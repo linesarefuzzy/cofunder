60842b9d0df1ef2e196daf480c6e699af86c712f
Adam Konner <adam.konner@gmail.com>
2013-06-27 14:43:34 -0400

    one more under_score relationship

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,5 @@
 class Country < ActiveRecord::Base
   include Legacy
-  belongs_to :Language, :foreign_key => 'LanguageID'
+  belongs_to :language, :foreign_key => 'LanguageID'
   attr_accessible :Name
 end
