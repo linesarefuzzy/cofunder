7ee4d5cd617c87325e534dc35b9390199c0e8e8a
Adam Konner <adam.konner@gmail.com>
2013-09-10 21:56:47 -0400

    loans.js

---
+++ app/assets/javascripts/loans.js
@@ -0,0 +1,14 @@
+$(function () {
+  // Javascript to enable link to tab
+  var hash = document.location.hash;
+  var prefix = "_";
+  if (hash)
+    $('.nav-tabs a[href='+hash.replace(prefix,"")+']').tab('show');
+  else
+    $('.nav-tabs a:first').tab('show'); // Select first tab
+
+  // Initiate slideshow
+  $('.carousel-inner .item').first().addClass('active')
+  $('.carousel-indicators li').first().addClass('active')
+  $('.carousel').carousel('fade')
+})
