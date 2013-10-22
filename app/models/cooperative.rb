59b2d76f4ed3db14171ef8796bf0f37b1631bf60
Adam Konner <adam.konner@gmail.com>
2013-10-22 13:23:21 -0400

    coop verbose name; empty gallery

--- app/models/cooperative.rb
+++ app/models/cooperative.rb
@@ -2,4 +2,8 @@ class Cooperative < ActiveRecord::Base
   include Legacy
   attr_accessible :Name
   has_many :loans, :foreign_key => 'CooperativeID'
+
+  def verbose_name
+    (self.name =~ /cooperative/i) ? self.name : "#{self.name} Cooperative"
+  end
 end
