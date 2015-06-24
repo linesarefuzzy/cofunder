1adc847f9bbfc9789ca1dbe9c02b0c202ab01f54
Adam Konner <adam.konner@gmail.com>
2015-06-24 19:12:58 -0400

    configure capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
+server 'cofunder.theworkingworld.org', user: 'adamk', roles: %w{app db web}
 
 
 
@@ -30,6 +28,9 @@
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
+
+set :deploy_to, '/var/rails/prod/cofunder'
+set :branch, 'production'
 
 
 
