d133b9401f8597392b5e826559ade671765fb33e
Adam Konner <adam.konner@gmail.com>
2015-06-24 19:29:55 -0400

    replace user with new deploy user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'cofunder.theworkingworld.org', user: 'adamk', roles: %w{app db web}
+server 'cofunder.theworkingworld.org', user: 'deploy', roles: %w{app db web}
 
 
 
