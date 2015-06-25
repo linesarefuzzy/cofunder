9c6c7ffb1158f14ff252be336cfd887bbabdfd85
Adam Konner <adam.konner@gmail.com>
2015-06-25 18:08:21 -0400

    linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, -> { "/var/rails/cofunder/#{fetch(:stage)}" }
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/initializers/local_config.rb}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/images public/assets}
